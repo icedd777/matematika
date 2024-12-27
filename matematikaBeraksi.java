9023083139dd86e580b8a7a15f92cd2fb59d9f47
icedd777 <dumatubunrisat@gmail.com>
2024-12-27 17:19:50 +0700

    Add files via upload

---
+++ matematikaBeraksi.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package matematika;
+
+/**
+ *
+ * @author Risat Dumatubun
+ */
+public class matematikaBeraksi {
+     public static void main(String[] args) {
+        matematika mtk = new matematika();
+        mtk.pertambahan(80, 100);
+        mtk.pengurangan(101, 40);
+        mtk.perkalian(20, 30);
+        mtk.pembagian(400, 400);
+    }
+    
+}
+    
+
